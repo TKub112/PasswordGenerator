1b5d9d9b861d5657aacacbd8171fdec9c59bb1af
TKub112 <tobiaszkubiak31@gmail.com>
2019-09-28 21:17:10 +0200

    Pnael added

---
+++ src/Swingpacket/AddPasswordRecord.java
@@ -0,0 +1,64 @@
+package Swingpacket;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Iterator;
+import java.util.Vector;
+
+import javax.swing.*; 
+
+final class AddPasswordRecord extends Ruuner {
+		static protected JButton addButton=new JButton("GeneratePassword");
+		Vector<JLabel> Labels = new Vector<JLabel>();
+		AddPasswordRecord()
+		{
+			//addButton.setBounds(25,25,100, 40);//x axis, y axis, width, height  
+			super.content.add(addButton);//adding button in JFrame  
+			addButton.addActionListener(new ActionListener(){  
+				public void actionPerformed(ActionEvent e){  
+						JLabel tf1=new JLabel(); 
+						Labels.add(tf1);
+						
+				        tf1.setBounds(50,0,150,20);
+				        content.add(tf1);
+				        System.out.println("hello");
+				        tf1.setFont(new java.awt.Font("Arial", Font.ITALIC, 16));
+				        tf1.setOpaque(true);
+				        tf1.setBackground(Color.WHITE);
+				        tf1.setForeground(Color.RED);
+				        SetBounder();
+				       
+				        }  
+				    });  
+		}
+		
+		final private void SetBounder()
+		{
+			 // Creating an iterator 
+	        Iterator<JLabel> currentLabel = Labels.iterator(); 
+	        int yRecordTmp = 0;
+	        while (currentLabel.hasNext()) { 
+	        	JLabel label = currentLabel.next();
+	        	label.setBounds(50,20+yRecordTmp,100, 40);
+	        	yRecordTmp+=20;
+	        	
+	        } 
+	        
+	        addButton.setBounds(50,50+yRecordTmp,100, 40);
+	        
+		}
+		
+		
+	
+		
+		/*public void actionPerformed(ActionEvent e){  
+		    JTextField tf1=new JTextField();  
+	        tf1.setBounds(50,50,150,20);
+	        f.add(tf1);
+	        }  
+	*/
+	
+	
+
+}
--- src/Swingpacket/Ruuner.java
+++ src/Swingpacket/Ruuner.java
@@ -1,23 +1,62 @@
 package Swingpacket;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JOptionPane;
+import javax.swing.JFrame;
 import javax.swing.*;  
 public class Ruuner extends BackEnd{ 
 	
 static protected JFrame f=new JFrame();
-
+static protected JButton b=new JButton("click");
+static protected JPanel content = new JPanel();
 public static void main(String[] args) {  
-	JButton b=new JButton("click");//creating instance of JButton  
-	b.setBounds(130,100,100, 40);//x axis, y axis, width, height  
-	          
-	f.add(b);//adding button in JFrame  
 	
-	f.setBounds(0,0,400,500);  //f.setBounds(x, y, width, height);
-	
+	new AddPasswordRecord();
 	new MenuBar();
 	
-	f.setLayout(null);//using no layout managers  
+	//JScrollPane pane = new JScrollPane(content);
+	//f.getContentPane().add(pane);
+	content.setBounds(20,20,300,350);//f.setBounds(x, y, width, height);
+	content.setMinimumSize(new Dimension(300,350));
+	content.setForeground(Color.RED);
+	content.setBackground(Color.WHITE);
 	
-	f.setVisible(true);//making the frame visible  
+	JScrollPane scrollableTextArea = new JScrollPane(content); 
+	scrollableTextArea.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);  
+	f.getContentPane().add(content);  
+	f.add(content);
+	f.setBounds(0,0,400,500);  //f.setBounds(x, y, width, height);
+	//f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	//f.setLayout(null);  
+	f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+    f.getContentPane().setLayout(new FlowLayout());  
+	f.setVisible(true);//making the frame visible 
+	
+	
+	/*Some piece of code*/
+	f.addWindowListener(new java.awt.event.WindowAdapter() {
+	    @Override
+	    public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+	       /* if (JOptionPane.showConfirmDialog(f, 
+	            "Are you sure you want to close this window?", "Close Window?", 
+	            JOptionPane.YES_NO_OPTION,
+	            JOptionPane.QUESTION_MESSAGE) == JOptionPane.YES_OPTION){
+	        	*/
+	        	try {
+					this.finalize();
+				} catch (Throwable e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+	            System.exit(0);
+	        }
+	    
+	});
 }  
 }  
 
